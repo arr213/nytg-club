0843a5bea8eeab54c87718dff7bb63bc4b50a291
Adam Reid <arr213@gmail.com>
2024-07-13 13:25:46 -0400

    added rules

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ import {
   Accordion, 
   AccordionActions, 
   AccordionSummary, 
-  AccordionDetails
+  AccordionDetails,
+  Modal
 } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {Info, ExpandMore, Close} from '@mui/icons-material';
 
 import PlayerSeasonChart from "./playerSeasonChart";
 
@@ -24,6 +25,7 @@ type GameFilter = {
 export default function Index() {
 
   const [records, setRecords] = useState<GameRecord[]>([]);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [filters, setFilters] = useState<{}>({});
   useEffect(() => {
     // Your code here
@@ -55,22 +57,24 @@ export default function Index() {
 
   return (
     <div className="font-sans antialiased bg-gray-100 h-screen flex flex-col w-dvw overflow-y-auto">
-        {/* <!-- Header --> */}
         <div className="bg-black text-white py-4 px-8 lg:text-center">
-            <h1 className="text-2xl font-bold">The Daily Puzzler</h1>
+          <h1 className="text-2xl font-bold">The Daily Puzzler</h1>
         </div>
 
         {/* <!-- Main Content --> */}
         <main className="flex-grow text-black md:w-1/2 mx-auto lg:w-1/3">
             {/* <!-- Weekly Scoreboard --> */}
             <section className="p-4 text-gray-600 text-center">
-                <h2 className="text-xl font-semibold mb-2">Weekly Standings</h2>
+                <h2 className="text-xl font-semibold mb-2">
+                  Weekly Standings
+                  <span className="ml-1" onClick={() => setModalOpen(true)}><Info /></span>
+                </h2>
                 <h3 className="text-lg font-semibold mb-2 ml-4">{league.getSeasonDates()[0]} - {league.getSeasonDates()[1]}</h3>
                 <div className="md:w-50">
                     {league.getSeasonScores().map((score) => {
                       return (
                           <Accordion key={score.player}>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <AccordionSummary expandIcon={<ExpandMore />}>
                               <div className="flex justify-between w-full">
                                 <h1 className="font-semibold">{score.player}</h1>
                                 <h1 className="font-semibold">{score.playerScore}</h1>
@@ -103,6 +107,34 @@ export default function Index() {
                 ))}
                 {/* <!-- Add more date sections dynamically --> */}
             </section>
+            <Modal open={modalOpen} onClose={() => setModalOpen(false)} className="bg-grey-200 p-4 mx-auto my-auto w-80/100 md:w-1/4">
+              <div className="bg-white p-4 mx-auto my-auto w-3/4 md:w-1/4">
+                <div className="flex justify-between">
+                  <h1 className="text-2xl font-semibold">League Info</h1>
+                  <Close onClick={() => setModalOpen(false)} />
+                </div>
+                <section className="p-4">
+                  <h2 className="text-xl">Join</h2>
+                  <p className="text-sm text-grey-600">To join the league, start emailing your NYT games scores to: arr213.nytg@valtown.email</p>
+                </section>
+                <section className="p-4 pt-0">
+                  <h2 className="text-xl">Scoring</h2>
+                  <p className="text-sm text-gray-600">We currently only accept Wordle, Connections, and Strands.</p>
+                  <p className="text-sm text-gray-600">A loss on any game is worth 1 point, so be sure to submit even if you lose.</p>
+                  <p className="text-sm text-gray-600">Scores reset weekly.</p>
+
+                  <h3 className="text-xl mt-2">Wordle</h3>
+                  <p className="text-sm text-gray-600">Wordle games receive 8 points minus the number of turns.</p>
+                  
+                  <h3 className="text-xl mt-2">Connections</h3>
+                  <p className="text-sm text-gray-600">Connections games are 9 points minus the number of turns.</p>
+
+                  <h3 className="text-xl mt-2">Strands</h3>
+                  <p className="text-sm text-gray-600">Strands games are 5 points minus the number of hints. (4 or more hints receive a score of 2)</p>
+
+                </section>
+              </div>
+            </Modal>
         </main>
     </div>
   );
